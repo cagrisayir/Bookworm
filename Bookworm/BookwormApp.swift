37ca98ceb977de07b85ed0671c81513944d11acf
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-09 00:45:05 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Omer Cagri Sayir on 9.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
